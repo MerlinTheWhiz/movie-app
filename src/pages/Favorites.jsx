18d56bc3cf2060190a38eb91776b9ae9ccbd8f68
MerlinTheWhiz <michaelnathan505@gmail.com>
2025-06-21 06:59:42 +0100

    first commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import "../css/Favorites.css"
+
+function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h2>No Favorites Yet</h2>
+            <p>Start adding to favorites and you'll see them here!</p>
+        </div>
+    )
+}
+
+export default Favorites
